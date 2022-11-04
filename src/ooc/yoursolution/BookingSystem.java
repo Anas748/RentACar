4a250b2c973ac5a99969334be83ed626035ffc3d
Anas748 <100359811+Anas748@users.noreply.github.com>
2022-11-04 00:10:43 +0000

    Update BookingSystem.java

--- src/ooc/yoursolution/BookingSystem.java
+++ src/ooc/yoursolution/BookingSystem.java
@@ -4,10 +4,19 @@
  */
 package ooc.yoursolution;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 /**
  *
  * @author user
  */
-public class BookingSystem {
-    
+public class BookingSystem implements BookingSystemInterface {
+   
+    @Override
+     public RentACarInterface setupRentACar(BufferedReader in) throws IOException{
+         
+        return null;
+         
+     }
 }
